af188b18e6673af0cb0017c56f9023c084e1a62f
Vinz Nguyen <nguyenhvubk@gmail.com>
2017-01-13 17:23:27 +0700

    fixing something

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,33 +8,12 @@ import { Task } from './model/task'
     styleUrls: [ 'app.component.css' ]
 })
 export class AppComponent {
-    private tasks = [
-        new Task(
-            "Buy a monkey",
-            false
-        ),
-        new Task(
-            "Walk the turtle",
-            false
-        ),
-        new Task(
-            "Walk the turtle",
-            false
-        ),
-        new Task(
-            "Walk the turtle",
-            false
-        ),
-        new Task(
-            "Walk the turtle",
-            false
-        )
-    ]
+    private tasks: Task[] = [];
+
     private currentTask = new Task(null,false);
 
     addTask() {
         let task = new Task(this.currentTask.content, this.currentTask.completed);
         this.tasks.push(task);
     }
-
  }
